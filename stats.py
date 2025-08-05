7624ccd7fe88a2472cd312961f90745b0f1ab170
Saranya <saranyaacbe@yahoo.com>
2025-08-05 16:33:18 +0530

    added testcases

--- stats.py
+++ stats.py
@@ -2,5 +2,12 @@
 def calculateStats(numbers):
   # implement the computation of statistics here and return the results
   stats = {}
-  stats['avg'] = sum (numbers)/len(numbers)
-  return None
+  if numbers:
+    stats['avg'] = sum (numbers)/len(numbers)
+    stats['max'] = max (numbers)
+    stats['min'] = min (numbers)
+  else:
+    stats['avg'] = float ('nan')
+    stats['max'] = float ('nan')
+    stats['min'] = float ('nan')
+  return stats
