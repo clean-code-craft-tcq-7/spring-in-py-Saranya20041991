3ea0dac2ab44a9d7f9dedbb32961ae5202fe5a69
Saranya <saranyaacbe@yahoo.com>
2025-08-12 15:48:06 +0530

    included testcases

--- stats.py
+++ stats.py
@@ -1,11 +1,13 @@
+import math
 
 def calculateStats(numbers):
   # implement the computation of statistics here and return the results
   stats = {}
-  if numbers:
-    stats['avg'] = sum (numbers)/len(numbers)
-    stats['max'] = max (numbers)
-    stats['min'] = min (numbers)
+  if numbers_without_nan:
+    numbers_without_nan = [num for num in numbers if not math.isnan9num0]
+    stats['avg'] = sum (numbers_without_nan)/len(numbers_without_nan)
+    stats['max'] = max (numbers_without_nan)
+    stats['min'] = min (numbers_without_nan)
   else:
     stats['avg'] = float ('nan')
     stats['max'] = float ('nan')
